305164448595d5885622196f33f80a690695d820
Jeff Bergman <jbergmanster@gmail.com>
2013-04-30 19:13:33 -0700

    Started adding a crude first implementation of Binomial Tree Pricing.

--- option.go
+++ option.go
@@ -4,13 +4,39 @@ import (
 	"math"
 )
 
-type OptType int
+// Position is whether this is a call or put.
+type Position int
 
 const (
-	Call = iota
+	Call Position = iota
 	Put
 )
 
-func Price(equityPrice, strikePrice float64, optType OptType) float64 {
+// Exercise determines when the option can be exercised.
+type Exercise int
+
+const (
+	American Exercise = iota
+	European
+)
+
+// Instrument is the type of option being priced.
+type Instrument struct {
+	P  Position
+	E  Exercise
+	Eq float64 // equity price at purchase
+	S  float64 // strike price
+}
+
+// BinSimParams are parameters use to price an option using the Binomial pricing model.
+type BinSimParams struct {
+	R float64 // interest rate
+	U float64 // up step
+	D float64 // down step
+	N int     // number of time steps
+}
+
+// BinomialPrice prices option using a Binomial tree.
+func BinomialPrice(inst Instrument, params BinSimParams) float64 {
 	return math.Pow(equityPrice, 2)
 }
